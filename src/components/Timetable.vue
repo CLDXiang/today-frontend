065aa1c0f808a9953b12e438c71b8969fe89a687
CLDXiang <417198670@qq.com>
2020-03-18 23:57:12 +0800

    timetable layout

---
+++ src/components/Timetable.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="timetable">
+    <div class="timetable__body">
+      <div class="timetable__time">
+        <div class="timetable__time__title"></div>
+        <div class="timetable__time__cell" v-for="(section, index) in sections" :key="index">{{ section }}</div>
+      </div>
+      <div class="timetable__day-box">
+        <time-table-day v-for="(title, index) in titles" :title="title" :key="index"></time-table-day>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import TimeTableDay from './TimetableDay.vue'
+
+export default {
+  props: {
+  },
+  data() {
+    return {
+      titles: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
+      sections: ['一', '二', '三', '四', '五', '六', '七', '八', '九', '十', '十一', '十二', '十三', '十四']
+    };
+  },
+  components: {
+    TimeTableDay
+  },
+  methods: {
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../scss/_timetable';
+
+.timetable {
+  height: 100%;
+  position: relative;
+
+  display: flex;
+  flex-direction: column;
+
+  margin: 10px;
+}
+
+.timetable__body {
+  display: flex;
+
+  overflow-x: auto;
+}
+
+.timetable__time {
+  position: sticky;
+  left: 0;
+
+  padding-right: 4px;
+
+  flex: 0 0 auto;
+
+  display: flex;
+  flex-direction: column;
+}
+
+.timetable__time__title {
+  flex: 1 0 $cell-height/2;
+  @include flex-center;
+
+  padding: 0 4px;
+
+}
+
+.timetable__time__cell {
+  flex: 1 0 $cell-height;
+  @include flex-center;
+
+  padding: 0 4px;
+  background: rgba(255, 255, 255, 0.5);
+
+  color: #69707a;
+  font-weight: 600;
+}
+
+.timetable__day-box {
+  display: flex;
+
+  border: 1px solid #ddd;
+  border-radius: 6px;
+}
+</style>
---
+++ src/components/TimetableDay.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="timetable__day">
+    <div class="timetable__day__title background-one">{{ title }}</div>
+    <div class="timetable__day__column-box">
+      <!-- TODO: 此处应根据 props 传下来的当日的课程计算该显示几列，并将每列该显示的课程信息作为 props 传给 column-->
+      <timetable-day-column></timetable-day-column>
+      <timetable-day-column></timetable-day-column>
+    </div>
+  </div>
+</template>
+
+<script>
+import TimetableDayColumn from './TimetableDayColumn.vue';
+
+export default {
+  props: {
+    title: String,
+  },
+  data() {
+    return {
+    };
+  },
+  components: {
+    TimetableDayColumn,
+  },
+  methods: {
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../scss/_timetable';
+
+.timetable__day {
+  height: 100%;
+  border-right: 1px solid #ddd;
+
+  flex: 1;
+
+  display: flex;
+  flex-direction: column;
+
+  // 最后一列不需要右边界
+  &:last-of-type {
+    border-right: 0;
+  }
+}
+
+.timetable__day__title {
+  flex: 1 0 $cell-height/2;
+  @include flex-center;
+  
+  color: #69707a;
+  font-weight: 600;
+}
+
+.timetable__day__column-box {
+  display: flex;
+}
+</style>

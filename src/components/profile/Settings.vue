6416067ca794985be717e288d67f5f16a7562653
gootter <gootter99@gmail.com>
2020-05-22 15:46:05 +0800

    fix(profile): change avatar by button

--- src/components/profile/Settings.vue
+++ src/components/profile/Settings.vue
@@ -11,12 +11,24 @@
         <v-avatar size="150" @click="clickAvatar">
           <img :src="processAvatar(avatar)" alt="avatar">
         </v-avatar>
+      </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-col cols="12" sm="8" md="6" lg="4">
         <v-file-input
-          v-show="false"
           id="input"
           v-model="newAvatar"
+          :rules="avatarRules"
           accept="image/png, image/jpeg, image/bmp"
-          @change="changeAvatar"
+          placeholder="Pick an avatar"
+          prepend-icon=""
+          prepend-inner-icon="mdi-camera"
+          label="头像"
+          show-size
+          clearable
+          outlined
+          rounded
+          @change="showAvatar"
         />
       </v-col>
     </v-row>
@@ -30,6 +42,7 @@
           clearable
           outlined
           rounded
+          :rules="nickNameRules"
           counter="30"
         />
       </v-col>
@@ -44,6 +57,7 @@
           rounded
           no-resize
           auto-grow
+          :rules="bioRules"
           counter="128"
         />
       </v-col>
@@ -66,51 +80,77 @@ import log from '../../utils/log';
 
 export default {
   data: () => ({
+    avatar: '',
+    bio: '',
+    nickName: '',
     newAvatar: [],
+    avatarRules: [(value) => !value || value.size < 200000 || '请使用不超过 200KB 的图片'],
+    nickNameRules: [(value) => !value || value.length <= 30 || '请确保昵称不超过30字'],
+    bioRules: [(value) => !value || value.length <= 128 || '请确保个性签名不超过128字'],
   }),
-  computed: mapState({
-    avatar: (state) => state.user.avatar,
-    bio: (state) => state.user.bio,
-    nickName: (state) => state.user.nickName,
-  }),
+  computed: {
+    ...mapState(['user']),
+  },
   created() {
     this.$store.commit('SET_BAR_TITLE', '用户设置');
+    this.fetchData();
   },
   methods: {
-    changeProfile() {
-      const UpdateUserDto = { nickName: this.nickName, bio: this.bio };
-      editProfile(UpdateUserDto)
-        .then((resp) => {
-          this.$store.commit('SET_USER_PROFILE', resp);
-          this.$message.success('更新完成');
-        })
-        .catch((err) => {
-          log.info(err);
-          this.$message.error('发生了错误');
-        });
+    fetchData() {
+      this.avatar = this.user.avatar;
+      this.bio = this.user.bio;
+      this.nickName = this.user.nickName;
     },
     clickAvatar() {
       this.$el.querySelector('#input').click();
     },
-    changeAvatar() {
-      const reader = new FileReader();
-      reader.onload = (data) => {
-        const res = data.target || data.srcElement;
-        this.avatar = res.result;
-      };
-      reader.readAsDataURL(this.newAvatar);
-      const dto = new FormData();
-      dto.append('file', this.newAvatar);
-      uploadAvatar(dto)
-        .then((resp) => {
-          this.$store.commit('SET_USER_PROFILE', resp);
-          this.$message.success('更新头像成功');
-        })
-        .catch((err) => {
-          log.info(err);
-          this.$message.error('请选择一张不超过200K的图片');
-          this.avatar = this.$store.state.user.avatar;
-        });
+    showAvatar() {
+      if (this.newAvatar === undefined) {
+        this.avatar = this.user.avatar;
+      } else {
+        const reader = new FileReader();
+        reader.onload = (data) => {
+          const res = data.target || data.srcElement;
+          this.avatar = res.result;
+        };
+        reader.readAsDataURL(this.newAvatar);
+      }
+    },
+    changeProfile() {
+      let result = true;
+      if (this.bio !== this.user.bio || this.nickName !== this.user.nickName) {
+        const UpdateUserDto = { nickName: this.nickName, bio: this.bio };
+        editProfile(UpdateUserDto)
+          .then((resp) => {
+            this.$store.commit('SET_USER_PROFILE', resp);
+          })
+          .catch((err) => {
+            log.info(err);
+            result = false;
+            // this.$message.error('发生了错误');
+          });
+      }
+      if (this.avatar !== this.user.avatar) {
+        const avatarDto = new FormData();
+        avatarDto.append('file', this.newAvatar);
+        uploadAvatar(avatarDto)
+          .then((resp) => {
+            this.$store.commit('SET_USER_PROFILE', resp);
+          })
+          .catch((err) => {
+            log.info(err);
+            result = false;
+            // this.$message.error('请选择一张不超过200K的图片');
+          });
+      }
+      if (result) {
+        this.$message.success('信息已更新，跳转个人主页……');
+        setTimeout(() => {
+          this.$router.push({ name: 'UserProfile' });
+        }, 1000);
+      } else {
+        this.$message.error('更新失败');
+      }
     },
     processAvatar(originAvatar) {
       // FIXME: 后端改掉对应默认图像路由后，这里只留下那一个路由的匹配串

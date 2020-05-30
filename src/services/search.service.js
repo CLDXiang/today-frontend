7f793d3813785a169bf43924d93730bd8a8635ac
lhq <lihongqinzhuanyong@gmail.com>
2020-05-30 14:48:03 +0800

    Add searching lecture/user in topbar

---
+++ src/services/search.service.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import log from '../utils/log';
+import store from '../store';
+import { API_URL } from '../utils/config';
+
+export function searchUser(str) {
+  const headers = {
+    Authorization: `Bearer ${store.state.user.jwt_token}`,
+  };
+  return new Promise((resolve, reject) => {
+    axios
+      .get(`${API_URL}/user`, { headers, params: { search: str } })
+      .then((resp) => {
+        log.info('GET user search resp', resp);
+        resolve(resp.data);
+      })
+      .catch(e => reject(e));
+  });
+}
+
+

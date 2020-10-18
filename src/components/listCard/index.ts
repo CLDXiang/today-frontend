1daf7527fbd456b8fad4f870033b2dddfd5608e9
CLDXiang <417198670@qq.com>
2020-10-18 19:46:41 +0800

    fix(lint): fix lint error caused by merge

--- src/components/listCard/index.ts
+++ src/components/listCard/index.ts
@@ -5,6 +5,5 @@ export { default as CardComment } from './CardComment.vue';
 export { default as CardLecture } from './CardLecture.vue';
 export { default as CardUser } from './CardUser.vue';
 export { default as CardReactiveRating } from './CardReactiveRating.vue';
-export { default as CardComment } from './CardComment.vue';
 
 export * from './types';

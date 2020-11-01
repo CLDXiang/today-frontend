be6e8f4a5be5de4e7fcf52d38affeebab0257a37
ahacad <1393345075@qq.com>
2020-11-01 18:44:09 +0800

    feat(utils/config): add jsonNameArray

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -30,6 +30,7 @@ export const semesterArray = [
   ['2018-2019学年暑期学期', '2018-2019学年暑期学期'],
   ['2019-2020学年1学期', '2019-2020学年1学期'],
   ['2019-2020学年2学期', '2019-2020学年2学期'],
+  ['2019-2020学年暑期学期', '2019-2020学年暑期学期'],
   ['2020-2021学年1学期', '2020-2021学年1学期'],
 ];
 
@@ -47,7 +48,27 @@ export const semesterNameArray = [
   '2018-2019学年暑期学期',
   '2019-2020学年1学期',
   '2019-2020学年2学期',
+  '2019-2020学年暑期学期',
   '2020-2021学年1学期',
+];
+
+export const jsonNameArray = [
+  'lessons_202_2015-2016_spring.json',
+  'lessons_222_2015-2016_summer.json',
+  'lessons_242_2016-2017_fall.json',
+  'lessons_243_2016-2017_spring.json',
+  'lessons_262_2016-2017_summer.json',
+  'lessons_263_2017-2018_fall.json',
+  'lessons_264_2017-2018_spring.json',
+  'lessons_284_2018-2019_fall.json',
+  'lessons_285_2018-2019_spring.json',
+  'lessons_287_2017-2018_summer.json',
+  'lessons_304_2018-2019_summer.json',
+  'lessons_324_2019-2020_fall.json',
+  'lessons_325_2019-2020_spring.json',
+  'lessons_326_2019-2020_winter.json',
+  'lessons_327_2019-2020_summer.json',
+  'lessons_344_2020-2021_fall.json',
 ];
 
 export const semesterObj = Object.fromEntries(semesterArray);

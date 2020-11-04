ff2b0f633a7c895945901502844524b018fe8765
CLDXiang <417198670@qq.com>
2020-11-04 17:21:49 +0800

    feat(axios): 引入 API 接口报错信息

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -2,24 +2,22 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { NODE_ENV } from './config';
 
-class Logger {
-  static log(...args: any[]) {
+const logger = {
+  log(...args: any[]) {
     if (NODE_ENV !== 'production') {
       console.log(...args);
     }
-  }
-
-  static info(...args: any[]) {
+  },
+  info(...args: any[]) {
     if (NODE_ENV !== 'production') {
       console.info(...args);
     }
-  }
-
-  static error(...args: any[]) {
+  },
+  error(...args: any[]) {
     if (NODE_ENV !== 'production') {
       console.error(...args);
     }
-  }
-}
+  },
+};
 
-export default Logger;
+export default logger;

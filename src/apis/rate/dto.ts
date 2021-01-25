d27b62af0fd3d72d4dde8bdbd94cd46ea74f400c
CLDXiang <cldxiang@qq.com>
2021-01-25 16:30:04 +0800

    refactor(apis): modify rate DTO

--- src/apis/rate/dto.ts
+++ src/apis/rate/dto.ts
@@ -1,4 +1,4 @@
-import { RateItem, RawRateResp } from './types';
+import { RateItem, RateFormDto, RateFormDtoPartial } from './types';
 
 /** GET rates Resp */
 export interface GetRatesRespDto {
@@ -7,93 +7,35 @@ export interface GetRatesRespDto {
 }
 
 /** POST rates Req */
-export interface PostRatesReqDto {
+export interface PostRatesReqDto extends RateFormDto {
   /** Lecture Id */
   lectureId: string;
-  /** 难易程度 */
-  difficulty?: number;
-  /** 给分好坏 */
-  nice?: number;
-  /** 工作量 */
-  workload?: number;
-  /** 综合推荐指数 */
-  recommended?: number;
-  /** 评价内容 */
-  content?: string;
-  /** 发布时间 */
-  updatedAt?: string;
-  /** 草稿 */
-  draft?: {
-    /** 难易程度 */
-    difficulty?: number;
-    /** 给分好坏 */
-    nice?: number;
-    /** 工作量 */
-    workload?: number;
-    /** 综合推荐指数 */
-    recommended?: number;
-    /** 评价内容 */
-    content?: string;
-    /** 发布时间 */
-    updatedAt: string;
-  }
 }
 
 /** POST rates Resp */
 export interface PostRatesRespDto {
   msg?: string;
-  data: RateItem[];
 }
 
-/** GET rates/:id Resp */
-export interface GetRatesIdRespDto {
-  data: RawRateResp;
+/** GET rates/:lectureId Resp */
+export interface GetRatesLectureIdRespDto {
+  msg?: string;
+  data: RateFormDto;
 }
 
-/** PATCH rates/:id Req */
-export interface PatchRatesIdReqDto {
-  /** 难易程度 */
-  difficulty?: number;
-  /** 给分好坏 */
-  nice?: number;
-  /** 工作量 */
-  workload?: number;
-  /** 综合推荐指数 */
-  recommended?: number;
-  /** 评价内容 */
-  content?: string;
-  /** 发布时间 */
-  updatedAt?: string;
-  /** 草稿 */
-  draft?: {
-    /** 难易程度 */
-    difficulty?: number;
-    /** 给分好坏 */
-    nice?: number;
-    /** 工作量 */
-    workload?: number;
-    /** 综合推荐指数 */
-    recommended?: number;
-    /** 评价内容 */
-    content?: string;
-    /** 发布时间 */
-    updatedAt: string;
-  }
+/** PATCH rates/:lectureId Req */
+export type PatchRatesLectureIdReqDto = RateFormDtoPartial;
+
+/** PATCH rates/:lectureId Resp */
+export interface PatchRatesLectureIdRespDto {
+  msg?: string;
 }
 
-/** PATCH rates/:id Resp */
-export interface PatchRatesIdRespDto {
-  /** 当前修改项数据 */
-  data: RateItem;
-}
+/** DELETE rates/:lectureId Req */
+export interface DeleteRatesLectureIdReqDto {}
 
-/** DELETE rates/:id Req */
-export interface DeleteRatesIdReqDto {
-  id: string;
-}
-
-/** DELETE rates/:id Resp */
-export interface DeleteRatesIdRespDto {
+/** DELETE rates/:lectureId Resp */
+export interface DeleteRatesLectureIdRespDto {
   msg?: string;
   /** 最新列表数据 */
   data: RateItem[];

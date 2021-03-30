8937d5e51867fb46be94c9f65a58d21f662084da
CLDXiang <417198670@qq.com>
2021-03-30 10:01:51 +0800

    fix(pwa): 修改打包内容缓存策略为 Network Frist

--- src/service-worker.js
+++ src/service-worker.js
@@ -4,9 +4,22 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerRoute(
-  /\.(?:png|gif|jpg|jpeg|svg|css|html|txt|js|json)$/,
+  /\.(?:png|gif|jpg|jpeg|svg|txt|json)$/,
   workbox.strategies.staleWhileRevalidate({
     cacheName: 'fdxk',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 240,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  }),
+);
+
+workbox.routing.registerRoute(
+  /\.(?:js|html|css)$/,
+  workbox.strategies.networkFirst({
+    cacheName: 'fdxk-bundle',
     plugins: [
       new workbox.expiration.Plugin({
         maxEntries: 240,

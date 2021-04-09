79f09807209f6fb91b21dcd87010f4f240ba47a2
ahacad <1393345075@qq.com>
2021-04-09 21:13:23 +0800

    feat(components): add rich-text input

---
+++ src/components/RichtextInput.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="border-2">
+    <span
+      ref="text"
+      role="textbox"
+      contenteditable
+      class="block w-full px-2 py-1 text-left"
+    />
+    <div class="flex justify-between h-8 py-2 bg-gray-50">
+      <div class="flex">
+        <f-icon
+          name="type-bold"
+          class="ml-1 mr-2 f-clickable"
+          size="16"
+        />
+        <f-icon
+          name="type-italic"
+          class="mr-2 f-clickable"
+          size="16"
+        />
+        <f-icon
+          name="strikethrough"
+          class="mr-2 f-clickable"
+          size="16"
+        />
+        <f-icon
+          name="code-slash"
+          class="mr-2 f-clickable"
+          size="16"
+        />
+        <f-icon
+          name="link-45deg"
+          class="mr-2 f-clickable"
+          size="16"
+        />
+        <f-icon
+          name="list-ol"
+          class="mr-2 f-clickable"
+          size="16"
+        />
+        <f-icon
+          name="list-ul"
+          class="mr-2 f-clickable"
+          size="16"
+        />
+        <f-icon
+          name="emoji-smile"
+          class="mr-2 f-clickable"
+          size="16"
+        />
+        <f-icon
+          name="paperclip"
+          class="mr-2 f-clickable"
+          size="16"
+        />
+      </div>
+      <f-icon
+        name="paper-plane"
+        class="mr-2 f-clickable"
+        size="16"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { FIcon } from '@/components/common';
+
+export default defineComponent({
+  components: {
+    FIcon,
+  },
+});
+</script>

4078b96596a518a6ebfc933ecaa06926a346e50d
CLDXiang <417198670@qq.com>
2021-04-10 19:38:58 +0800

    feat(f-button): add icon slot

--- src/components/common/FButton.vue
+++ src/components/common/FButton.vue
@@ -27,7 +27,11 @@
         d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
       />
     </svg>
-    <div :class="{ 'opacity-0': loading }">
+    <div
+      class="flex items-center"
+      :class="{ 'opacity-0': loading }"
+    >
+      <slot name="icon" />
       <slot />
     </div>
   </button>

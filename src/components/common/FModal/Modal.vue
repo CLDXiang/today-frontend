928e9c3a025ab3660a1e0fb8b5de6e3cf7bb2802
CLDXiang <417198670@qq.com>
2021-04-13 12:22:45 +0800

    feat(f-modal): add mask logic

--- src/components/common/FModal/Modal.vue
+++ src/components/common/FModal/Modal.vue
@@ -1,39 +1,46 @@
 <template>
   <teleport to="body">
-    <div
-      v-if="isVisible"
-      class="fixed top-0 left-0 w-full h-full bg-black bg-opacity-40 z-40 flex justify-center items-start"
-    >
+    <div class="w-0 h-0 fixed top-0 left-0 overflow-visible">
       <div
-        class="relative z-50 top-24 mx-4 bg-white rounded shadow-lg"
-        style="min-width: 240px; max-width: 520px"
+        v-if="mask && isVisible"
+        class="fixed top-0 left-0 w-full h-full bg-black bg-opacity-40 z-40 flex justify-center items-start"
+        @click="handleClickMask"
+      />
+      <div
+        v-if="isVisible"
+        class="fixed top-0 left-1/2 w-0 h-0 z-50 flex justify-center items-start"
       >
-        <div class="w-full flex justify-between text-base font-medium">
-          <span class="flex-auto px-6 py-4 text-gray-800">{{ title }}</span>
-          <span
-            v-if="closable"
-            class="flex-shrink-0 w-14 h-14 flex justify-center items-center f-clickable"
-            @click="setIsVisible(false)"
-          >
-            <f-icon
-              name="close"
-              size="16"
-            />
-          </span>
-        </div>
-        <div class="w-full p-6 text-sm text-gray-600">
-          <slot />
-        </div>
-        <div class="w-full px-4 py-2.5 flex justify-end space-x-2">
-          <f-button @click="handleCancel">
-            {{ cancelText }}
-          </f-button>
-          <f-button
-            type="primary"
-            @click="handleOk"
-          >
-            {{ okText }}
-          </f-button>
+        <div
+          class="relative top-24 mx-4 bg-white rounded shadow-xl"
+          style="min-width: 240px; max-width: 520px"
+        >
+          <div class="w-full flex justify-between text-base font-medium">
+            <span class="flex-auto px-6 py-4 text-gray-800">{{ title }}</span>
+            <span
+              v-if="closable"
+              class="flex-shrink-0 w-14 h-14 flex justify-center items-center f-clickable"
+              @click="setIsVisible(false)"
+            >
+              <f-icon
+                name="close"
+                size="16"
+              />
+            </span>
+          </div>
+          <div class="w-full p-6 text-sm text-gray-600">
+            <slot />
+          </div>
+          <div class="w-full px-4 py-2.5 flex justify-end space-x-2">
+            <f-button @click="handleCancel">
+              {{ cancelText }}
+            </f-button>
+            <f-button
+              type="primary"
+              @click="handleOk"
+            >
+              {{ okText }}
+            </f-button>
+          </div>
         </div>
       </div>
     </div>
@@ -95,11 +102,19 @@ export default defineComponent({
       setIsVisible(false);
     };
 
+    /** 点击遮罩回调 */
+    const handleClickMask = () => {
+      if (props.maskClosable) {
+        handleCancel();
+      }
+    };
+
     return {
       isVisible,
       setIsVisible,
       handleCancel,
       handleOk,
+      handleClickMask,
     };
   },
 });
